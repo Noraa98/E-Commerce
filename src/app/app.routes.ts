52d54e0ee816c918166e370322a37682a9c0dca6
Noraa98 <badr91780@gmail.com>
2025-09-29 09:25:38 +0200

    add Angular routing , configure styles ,setup project structure (core, shared, features) and create footer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,62 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from '../core/layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from '../core/layouts/blank-layout/blank-layout.component';
+import { LoginComponent } from '../core/auth/login/login.component';
+import { RegisterComponent } from '../core/auth/register/register.component';
+import { HomeComponent } from '../core/features/home/home.component';
+import { CartComponent } from '../core/features/cart/cart.component';
+import { ProductsComponent } from '../core/features/products/products.component';
+import { BrandsComponent } from '../core/features/brands/brands.component';
+import { CategoriesComponent } from '../core/features/categories/categories.component';
+import { DetailsComponent } from '../core/features/details/details.component';
+import { CheckoutComponent } from '../core/features/checkout/checkout.component';
+import { NotfoundComponent } from '../core/features/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path:'' ,redirectTo:"home",pathMatch:'full'
+    },
+    {
+        path:'' ,
+        component:AuthLayoutComponent,
+        children :[
+            {
+                path:'login' ,component:LoginComponent ,title:'login Page'
+            },
+            {
+                path:'register' ,component:RegisterComponent ,title:'register Page'
+            }
+        ]
+    },
+    {
+        path:'' ,
+        component:BlankLayoutComponent,
+        children :[
+            {
+                path:'home' ,component:HomeComponent ,title:'home Page'
+            },
+            {
+                path:'cart' ,component:CartComponent ,title:'cart Page'
+            },
+            {
+                path:'products' ,component:ProductsComponent ,title:'products Page'
+            },
+            {
+                path:'brands' ,component:BrandsComponent ,title:'brands Page'
+            },
+            {
+                path:'categories' ,component:CategoriesComponent ,title:'categories Page'
+            },
+            {
+                path:'details' ,component:DetailsComponent ,title:'categories Page'
+            },
+            {
+                path:'checkout' ,component:CheckoutComponent ,title:'chekout Page'
+            }
+        ]
+    },
+    {
+        path:'**' ,component:NotfoundComponent , title:'notfound Page'
+    }
+];
