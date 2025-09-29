b1f6ca55006f54d927b3339d2a7e3f83b5014cd4
Noraa98 <badr91780@gmail.com>
2025-09-29 10:41:56 +0200

    add navbar component

--- src/core/shared/components/navbar/navbar.component.ts
+++ src/core/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FlowbiteService } from '../../../services/flowbite.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  constructor(private flowbiteService: FlowbiteService) {}
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
