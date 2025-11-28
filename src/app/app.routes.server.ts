dd6b6b2d9b5e1982c3faf9182fe0bc3ca341a21c
Noraa98 <badr91780@gmail.com>
2025-11-28 19:56:48 +0200

    Ready for Vercel deployment

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,28 @@
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { appConfig } from './app.config';
 
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering()
+  ]
+};
+export const config = mergeApplicationConfig(appConfig, serverConfig);
 export const serverRoutes: ServerRoute[] = [
+  {
+  path: 'details/:slug/:id',
+    renderMode: RenderMode.Client 
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Client 
+  },
+  
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
+
+
